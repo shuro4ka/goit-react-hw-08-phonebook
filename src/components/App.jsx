597a1a09027c54eb1afdba5eafcb18ee5a39219b
shuro4ka <zzlesia77@gmail.com>
2022-12-24 20:25:22 +0000

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,19 @@ export class App extends Component {
     filter: ''
   }
 
+  componentDidMount(){
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if(contacts){
+      this.setState({contacts: contacts});
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(this.state.contacts !== prevState.contacts){
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (name, number) => {
     const isExist = this.state.contacts.find((contact) => { 
       if(contact.name === name || contact.number === number) {
